3f4803c15de2bddf5d7dab1e204ab04008a4d43a
Keegan Oliver Atchison <keeganatchison@gmail.com>
2026-03-04 21:11:18 -0500

    Fix custom-domain Pages config and CNAME setup

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/me/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
         main: fileURLToPath(new URL('./index.html', import.meta.url)),
         art: fileURLToPath(new URL('./project pages/art.html', import.meta.url)),
         biomaterial3DP: fileURLToPath(new URL('./project pages/biomaterial3DP.html', import.meta.url)),
+        context: fileURLToPath(new URL('./project pages/context.html', import.meta.url)),
         geopolymer: fileURLToPath(new URL('./project pages/geopolymer.html', import.meta.url)),
         MN: fileURLToPath(new URL('./project pages/MN.html', import.meta.url)),
         tapestry: fileURLToPath(new URL('./project pages/tapestry.html', import.meta.url)),
